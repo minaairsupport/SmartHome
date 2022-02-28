7a78a7394b817920d6b35757f1c0cc276e890122
Mina Fawzy <gentel_mina2011@yahoo.com>
2022-02-28 15:48:10 +0200

    create splash screen + make app accept view biding + create splash theme

---
+++ app/src/main/java/com/massive/smarthome/ui/component/splash/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.massive.smarthome.ui.component.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.massive.smarthome.databinding.SplashLayoutBinding
+import com.massive.smarthome.ui.base.BaseActivity
+
+class SplashActivity : BaseActivity() {
+
+    private lateinit var binding: SplashLayoutBinding
+    val SPLASH_DELAY = 3000
+
+
+    override fun initViewBinding() {
+        binding = SplashLayoutBinding.inflate(layoutInflater)
+        val view =  binding.root
+        setContentView(view)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navigateToHomePage()
+    }
+
+    private fun navigateToHomePage() {
+        Handler(Looper.getMainLooper()).postDelayed({
+
+//            val nextScreenIntent = Intent(this, LoginActivity::class.java)
+//            startActivity(nextScreenIntent)
+//            finish()
+        }, SPLASH_DELAY.toLong())
+    }
+
+    override fun observeViewModel() {
+    }
+}
