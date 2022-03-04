0ce002e98cd192ee6e5952eb2cac5127421f8fd6
Mina Fawzy <gentel_mina2011@yahoo.com>
2022-03-04 17:29:59 +0200

    add some Dao

--- app/src/main/java/com/massive/smarthome/data/local/AppDao.kt
+++ app/src/main/java/com/massive/smarthome/data/local/AppDao.kt
@@ -1,8 +1,55 @@
 package com.massive.smarthome.data.local
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.massive.smarthome.data.dto.Address
+import com.massive.smarthome.data.dto.DevicesItem
+import com.massive.smarthome.data.dto.User
 
 @Dao
 interface AppDao {
     // all database methods should be here
+
+    // Queries
+    @Query("SELECT * FROM device WHERE productType = :type")
+    fun getDevicesByType(type: String): LiveData<List<DevicesItem>>
+
+    @Query("SELECT * FROM device ORDER BY productType ASC")
+    fun getAllDevicesOrderedByType(): LiveData<List<DevicesItem>>
+
+    @Query("SELECT * FROM device ORDER BY id ASC")
+    fun getAllDevicesOrderedById(): LiveData<List<DevicesItem>>
+
+    @Query("SELECT * FROM user")
+    fun getUser(): LiveData<User>
+
+    @Query("SELECT * FROM address")
+    fun getAddress(): LiveData<Address>
+
+
+    //Insertion
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllDevices(devices: List<DevicesItem>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user : User)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAddress(address : Address)
+
+    // Delete
+    @Query("DELETE FROM device")
+    fun deleteAllDevices()
+
+    @Query("DELETE FROM device WHERE id = :id")
+    fun deleteDeviceById(id: Int)
+
+    @Delete
+    fun deleteDeviceByObj(device: DevicesItem)
+
+    @Query("DELETE FROM user")
+    fun deleteUser()
+
+    @Query("DELETE FROM address")
+    fun deleteAddress()
 }
